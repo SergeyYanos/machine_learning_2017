95793c4058466d95d90559f089af40ab03c56193
Sergey Yanos <sergey.yanos@gmail.com>
2017-04-11 16:54:55 +0300

    blala

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -2,6 +2,7 @@ import pandas
 import numpy
 import numbers
 import os
+import sys
 
 
 def dist(x, y, r):
@@ -16,30 +17,44 @@ def dist(x, y, r):
     return d
 
 
-# Task no. 1: Load the Election Challenge data from the ElectionsData.csv file
-csv_file_path = os.path.join(os.getcwd(), "ElectionsData.csv")
-data = pandas.read_csv(csv_file_path)
+def set_correct_types(data_frame):
+    obj_features = data_frame.keys()[data_frame.dtypes.map(lambda x: x == 'object')].drop('Vote')
+    for f in obj_features:
+        data_frame[f] = data_frame[f].astype("category")
+        data_frame[f + "Int"] = data_frame[f].cat.rename_categories(range(data_frame[f].nunique())).astype(int)
+        data_frame.loc[data_frame[f].isnull(), f + "Int"] = numpy.nan
 
-# Task no. 2: Identify and set the correct type of each attribute
-# TODO.
+    not_obj_features = data_frame.keys()[data_frame.dtypes.map(lambda x: x != 'object')]
+    for f in not_obj_features:
+        if pandas.unique(data_frame[f]).size < 1000:
+            data_frame[f] = data_frame[f].astype("category")
+            data_frame[f] = data_frame[f].cat.rename_categories(range(data_frame[f].nunique())).astype(int)
+            data_frame.loc[data_frame[f].isnull(), f] = sys.maxint
 
-# Task no. 3: Perform the following data preparation tasks using ALL the data
-# 1. Imputation
 
-print data[data.Occupation_Satisfaction.isnull()].size
+def impute_data(data_frame):
+    print data_frame[pandas.isnull(data_frame).any(axis=1)]
+    # for i, v in data_frame[data_frame.isnull()].iterrows():
+    #     closest_i = -1
+    #     min_dist = sys.maxint
+    #     r = 1
+    #     if data[attr].dtype != 'object':
+    #         r = data[attr].max() - data[attr].min()
+    #     for i1, v1 in data.iterrows():
+    #         if i != i1:
+    #             d = dist(v, v1, r)
+    #             if d < min_dist:
+    #                 closest_i, min_dist = i1, d
+    #     v[attr] = data.iloc[closest_i][attr]
 
-for attr in ['Occupation_Satisfaction']:
-    for i, v in data[data[attr].isnull()].iterrows():
-        closest_i = -1
-        min_dist = 100000
-        r = 1
-        if data[attr].dtype != 'object':
-            r = data[attr].max() - data[attr].min()
-        for i1, v1 in data.iterrows():
-            if i != i1:
-                d = dist(v, v1, r)
-                if d < min_dist:
-                    closest_i, min_dist = i1, d
-        v[attr] = data.iloc[closest_i][attr]
+if __name__ == "__main__":
+    # Task no. 1: Load the Election Challenge data from the ElectionsData.csv file
+    csv_file_path = os.path.join(os.getcwd(), "ElectionsData.csv")
+    data = pandas.read_csv(csv_file_path)
 
-print data[data.Occupation_Satisfaction.isnull()].size
+    # Task no. 2: Identify and set the correct type of each attribute.
+    set_correct_types(data)
+
+    # Task no. 3: Perform the following data preparation tasks using ALL the data
+    # Imputation:
+    impute_data(data)
