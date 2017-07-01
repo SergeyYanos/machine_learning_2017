2eec9577990954da33e38c326c51ea2db225b544
sergeyy <sergey.yanos@gmail.com>
2017-07-01 18:21:24 +0300

    First commit.
    Signed-off-by: sergeyy <sergey.yanos@gmail.com>

---
+++ hw5/hw5.py
@@ -0,0 +1,16 @@
+from utils import *
+
+
+@timed
+def main():
+    pass
+    #  load the data
+    #  train models(one for predicting labels and another for clustering)
+    #  predict label for each voter
+    #  get division of votes
+    #  get winning party
+    #  get steady coalition
+
+
+if __name__ == "__main__":
+    main()
